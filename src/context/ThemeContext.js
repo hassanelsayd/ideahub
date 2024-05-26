0ff90643a746e6546d19bb7c2f6a0a7809195902
Hassan Elsayd <hassan22elsayd@gmail.com>
2024-05-26 17:27:34 +0300

    update theme context

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -4,7 +4,7 @@ import { createContext, useEffect, useState } from "react";
 export const ThemeContext = createContext();
 
 const getFromLocalStorage = () => {
-  if (window !== undefined) {
+  if (typeof window !== "undefined") {
     const value = window.localStorage.getItem("theme");
     return value || "light";
   }
